d1957503738558bf4fa7cad7d708778b6ecd8935
Celeste Robinson <crobinso@adobe.com>
2018-08-01 16:30:41 -0600

    google ads complete, needs testing

--- src/lib/actions/trackConversion.js
+++ src/lib/actions/trackConversion.js
@@ -1,5 +1,13 @@
 'use strict';
+var loadGtag = turbine.getSharedModule('gtag.js', 'loadGtag');
+var extensionSettings = turbin.getExtensionSettings();
+var options = {
+  send_page_view: !extensionSettings.preventAddingVisitor,
+  conversion_linker: !extensionSettings.preventCookies
+};
+loadGtag();
+gtag('config', extensionSettings.conversionId, options );
 
 module.exports = function(settings) {
-  // TODO Perform some action.
+  gtag('event', 'conversion', {'send_to': extensionSettings.conversionId + '/' + extensionSettings.conversionLabel});
 };
