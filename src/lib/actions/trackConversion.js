08cd458dfccca71b2cccd446d82f4380d9db923e
Celeste Robinson <crobinso@adobe.com>
2018-08-16 12:10:12 -0600

    added basic styling to action config view

--- src/lib/actions/trackConversion.js
+++ src/lib/actions/trackConversion.js
@@ -1,12 +1,9 @@
 'use strict';
-alert("We got to trackconversion.js")
 
 var loadGtag = turbine.getSharedModule('gtag.js', 'loadGtag');
 loadGtag();
-alert("We got passed loading gtag")
 
 module.exports = function(settings) {
-  alert("we got inside this function");
   gtag('config', settings.conversionId);
   gtag('event', 'conversion', {
     'send_to': settings.conversionId + '/' + settings.conversionLabel,
@@ -14,5 +11,4 @@ module.exports = function(settings) {
     'currencyCode': settings.currencyCode,
     'orderId': settings.orderId
   });
-  alert("we finished");
 };
