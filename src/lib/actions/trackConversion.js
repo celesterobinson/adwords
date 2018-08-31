49cef7ce0f9e2f18aee03055890ba3aa02ca49c9
Celeste Robinson <crobinso@adobe.com>
2018-08-31 16:31:56 -0600

    adding conversion value, currency code, and order id to object only if it exists

--- src/lib/actions/trackConversion.js
+++ src/lib/actions/trackConversion.js
@@ -1,14 +1,22 @@
 'use strict';
 
-module.exports = function(settings) {
-  if(!gtag) {
+module.exports = function (settings) {
+
+  if (!gtag) {
     turbine.logger.error('It appears you do not have gtag.js loaded onto your site. Please go to the extension catalog, and download the gtag.js extension package. You will need to add your Google Ads conversion ID.')
   } else {
-    gtag('event', 'conversion', {
-      'send_to': settings.conversionId + '/' + settings.conversionLabel,
-      'conversionValue': Number(settings.conversionValue),
-      'currencyCode': settings.currencyCode,
-      'orderId': settings.orderId
-    });
+    var eventData = {
+      'send_to': settings.conversionId + '/' + settings.conversionLabel
+    };
+    if (settings.conversionValue) {
+      eventData.conversionValue = settings.conversionValue
+    }
+    if (settings.currencyCode) {
+      eventData.currencyCode = settings.currencyCode
+    }
+    if (settings.orderId) {
+      eventData.orderId = settings.orderId
+    }
+    gtag('event', 'conversion', eventData);
   }
 };
